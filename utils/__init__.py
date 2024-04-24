42b7fec2ac85367a53b51350d4861e95b50e8911
Amen Wondwosen <amenwond408@gmail.com>
2024-04-24 16:29:45 -0700

    Added logger (not implemented)

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def get_logger(name) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
