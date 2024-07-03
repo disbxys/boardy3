ea1292bd7c9b1e4849aed7aff9a08e7626736577
disbxys <amenwond408@gmail.com>
2024-07-03 16:29:43 -0700

    Add more tests for tags

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,6 +5,7 @@ import shutil
 import unittest
 
 from boardy3.database.database_manager import DatabaseManager
+import boardy3.database.exceptions as db_exc
 
 
 class TestDB(unittest.TestCase):
@@ -87,7 +88,15 @@ class TestDB(unittest.TestCase):
 
 
     def test_fail_add_existing_tag(self):
-        raise NotImplementedError
+        tag_name = "test_tag"
+
+        self.db_manager.add_tag(tag_name)
+
+        with self.assertRaises(
+            db_exc.DatabaseItemExists,
+            msg="Tag should already exist but was not found."
+        ):
+            self.db_manager.add_tag(tag_name)
 
 
     def test_add_tag_to_image(self):
@@ -130,32 +139,117 @@ class TestDB(unittest.TestCase):
 
     
     def test_add_tags_to_image(self):
-        raise NotImplementedError
+        # Get a random test image
+        _image = random.choice(self.test_images)
+
+        # Add image to database
+        self.db_manager.add_image(_image)
+
+        # Grab newly added image
+        db_image = self.db_manager.get_all_images(newest_first=True)[0]
+
+        # Create a test tag
+        # test_tag1 = self.db_manager.add_tag("test_tag1")
+        # test_tag2 = self.db_manager.add_tag("test_tag2")
+        # test_tag3 = self.db_manager.add_tag("test_tag3")
+        test_tag_set = {
+            self.db_manager.add_tag("test_tag1"),
+            self.db_manager.add_tag("test_tag2"),
+            self.db_manager.add_tag("test_tag3")
+        }
+
+        # Test if tag successfully added to image
+        for test_tag in test_tag_set:
+            self.assertTrue(self.db_manager.add_tag_to_image(test_tag, db_image.id))
+
+        # Replace db image with copy from database. It should safe to not
+        # add a test for it since it's a part of another test.
+        db_image = self.db_manager.get_all_images(newest_first=True)[0]
+
+        self.assertTrue(
+            set(db_image.tags).issuperset(test_tag_set),
+            f"Tag has not been added to test image."
+        )
     
 
     def test_fail_add_tag_to_nonexisting_image(self):
-        raise NotImplementedError
+        # Create a test tag
+        tag_ = self.db_manager.add_tag("test_tag")
+
+        # Test if tag successfully added to image
+        self.assertRaises(
+            db_exc.DatabaseItemDoesNotExist,
+            self.db_manager.add_tag_to_image,
+            tag_, 9999
+        )
     
 
     def test_delete_tag(self):
-        raise NotImplementedError
+        tag_ = self.db_manager.add_tag("test_tag")
+
+        self.db_manager.delete_tag(tag_.id)
+        
+        self.assertIsNone(self.db_manager.get_tag_by_name(tag_.name))
     
 
     def test_delete_nonexisting_tag(self):
-        raise NotImplementedError
+        self.assertRaises(
+            db_exc.DatabaseItemDoesNotExist,
+            self.db_manager.delete_tag, 2
+        )
     
 
     def test_delete_all_tags(self):
-        raise NotImplementedError
+        self.db_manager.delete_all_tags()
+
+        self.assertEqual(
+            len(self.db_manager.search_tags()), 0,
+            "There should be no tags."
+        )
     
 
     def test_remove_tag_from_image(self):
-        raise NotImplementedError
+        # Get a random test image
+        _image = random.choice(self.test_images)
+
+        # Add image to database
+        self.db_manager.add_image(_image)
+
+        # Grab newly added image
+        db_image = self.db_manager.get_all_images(newest_first=True)[0]
+
+        # Create a test tag
+        tag_ = self.db_manager.add_tag("test_tag")
+
+        # Add tag to image
+        self.db_manager.add_tag_to_image(tag_, db_image.id)
+
+        # Remove tag from image
+        self.db_manager.remove_tag_from_image(tag_.id, db_image.id)
+
+        # Re-grab image from database
+        db_image = self.db_manager.get_image(db_image.id)
+
+        self.assertIsNotNone(db_image)
+
+        self.assertFalse(tag_ in db_image.tags) # type: ignore
     
 
     def test_remove_tags_from_image(self):
         raise NotImplementedError
     
+
+    def test_fail_remove_invalid_tag_from_image(self):
+        raise NotImplementedError
+    
+
+    def test_fail_remove_tag_from_invalid_image(self):
+        raise NotImplementedError
+    
+
+    def test_fail_remove_invalid_tag_from_invalid_image(self):
+        raise NotImplementedError
+
 
     def tearDown(self) -> None:
         # Clear all images and tags from database
